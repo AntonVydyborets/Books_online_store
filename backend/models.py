4303e9a6cc197779b28d4eeb840f90bd858e26af
Anton-bat <131353970+Anton-bat@users.noreply.github.com>
2024-12-04 22:36:26 +0200

    com2 models added

---
+++ backend/models.py
@@ -0,0 +1,43 @@
+from sqlalchemy import Boolean, Integer, Column, String, Float, ForeignKey, Text
+from sqlalchemy.orm import relationship
+from backend.database import Base
+
+class Book(Base):
+    __tablename__ = "books"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String(100), index=True, nullable=False)
+    description = Column(Text, nullable=True)
+    price = Column(Float, nullable=False)
+    publication_year = Column(Integer, nullable=False)
+    country_of_origin = Column(String(50), nullable=True)
+    text_language = Column(String(50), nullable=True)
+    category = Column(String(50), nullable=False)
+    rating = Column(Float, nullable=True, default=0.0)
+    is_available = Column(Boolean, default=True)
+
+    order_books = relationship("OrderBook", back_populates="book")
+
+
+class Orders(Base):
+    __tablename__ = "orders"
+
+    id = Column(Integer, primary_key=True, index=True)
+    status = Column(String(50), default="Pending")
+    total_price = Column(Float, nullable=False)
+    email = Column(String(100), nullable=False)
+    phone = Column(String(15), nullable=False)
+
+    items = relationship("OrderBook", back_populates="order")
+
+
+class OrderBook(Base):
+    __tablename__ = "order_books"
+
+    id = Column(Integer, primary_key=True, index=True)
+    order_id = Column(Integer, ForeignKey("orders.id"), nullable=False)
+    book_id = Column(Integer, ForeignKey("books.id"), nullable=False)
+    quantity = Column(Integer, default=1)
+    order = relationship("Orders", back_populates="items")
+    book = relationship("Book", back_populates="order_books")
+
