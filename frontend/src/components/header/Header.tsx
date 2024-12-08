ef3331117a12b862e960771d291b7d6396ec5a3d
Nazar <ups14q@gmail.com>
2024-12-08 13:23:41 +0200

    base vertical menu content

--- frontend/src/components/header/Header.tsx
+++ frontend/src/components/header/Header.tsx
@@ -3,6 +3,8 @@ import burger from '@/assets/images/burger.svg'
 import account_icon from '@/assets/images/account.svg'
 import wishlist_icon from '@/assets/images/wishlist.svg'
 import trash_icon from '@/assets/images/trash.svg'
+import star_icon from '@/assets/images/Star.svg'
+import right_arrow from '@/assets/images/chevron-right.svg'
 
 import Container from '@/shared/container/Container'
 
@@ -51,6 +53,21 @@ const Header = () => {
               <img src={burger} alt="burger" />
             </div>
             <div className={s.verticalNavigation__title}>Каталог</div>
+            <div className={s.verticalNavigation__additionalContent}>
+              <ul className="additionalContent__menu">
+                <li className={s.additionalContent__menu__item}>
+                  <a className={s.additionalContent__menu__item__link} href="#">
+                    <div className={s.starIcon}>
+                      <img src={star_icon} alt="star-icon" />
+                    </div>
+                    <span className={s.menu__item__link__text}>Паперові книги</span>
+                    <div>
+                      <img className="arrowIcon" src={right_arrow} alt="right-arrow" />
+                    </div>
+                  </a>
+                </li>
+              </ul>
+            </div>
           </div>
           <div className="header_bottom__menu">
             <ul className={s.header_bottom__menu__main}>
