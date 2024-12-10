41069bdb5464997512c38f528eff93e3ac900ee5
Masianuy <Masianuy@ukr.net>
2024-12-10 15:05:00 +0200

    added breakpoints to sliders

--- frontend/src/components/sliderBooks/SliderBooks.tsx
+++ frontend/src/components/sliderBooks/SliderBooks.tsx
@@ -20,7 +20,23 @@ const SliderBooks: React.FC<SliderBooksProps> = ({ title, data }) => {
         slidesPerView={4}
         loop={true}
         navigation={true}
-        modules={[Navigation]}>
+        modules={[Navigation]}
+        breakpoints={{
+          440: {
+            slidesPerView: 1,
+          },
+          674: {
+            slidesPerView: 2,
+          },
+          901: {
+            slidesPerView: 3,
+          },
+          1117: {
+            slidesPerView: 4,
+            spaceBetween: 10,
+          },
+        }}
+        >
         {data.map((book) => (
           <SwiperSlide key={book.id}>
             <SlideItem book={book} />
