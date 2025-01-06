e6528187e6abebf03c4d40ffd783ef609f7aa0e7
Masianuy <Masianuy@ukr.net>
2025-01-06 19:56:35 +0200

    change styled to flex

--- frontend/src/components/checkoutCard/СheckoutCard.tsx
+++ frontend/src/components/checkoutCard/СheckoutCard.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { OrderType } from '@/utils/types/OrderType'
 import FirstStep from './FirstStep'
 import SecondStep from './SecondStep'
@@ -6,7 +6,6 @@ import ThirdStep from './ThirdStep'
 import Book from './Book'
 import { useProductsStore } from '@/store/useProductsStore.ts'
 import s from './СheckoutCard.module.scss'
-
 import iconsReturning from '@/assets/images/icons-returning.svg'
 import iconsFreeDeliver from '@/assets/images/icons-free-deliver.svg'
 import iconSafety from '@/assets/images/icon-safety.svg'
@@ -15,14 +14,24 @@ const СheckoutCard = () => {
   const orders = useProductsStore((state) => state.orders)
   const [step, setStep] = useState<number>(1)
   const [order, setOrder] = useState(orders)
+  const [totalPrice, setTotalPrice] = useState<number>(0)
+  const countPrice = (arr) => {
+    let res: number = 0;
+    arr?.orderItems.forEach(item => res += item?.book?.price)
+    return setTotalPrice(res)
+  }
   const nextStep = () => {
     setStep((step) => step + 1)
   }
 
+  useEffect(() => {
+    countPrice(order)
+  }, [order])
+
   return (
     <>
       <section className={s.container}>
-        <div className={s.grid}>
+        <div className={s.wrapper}>
           <div className={s.left}>
             {step === 1 && <FirstStep order={order} nextStep={nextStep} />}
             {step === 2 && <SecondStep nextStep={nextStep} />}
@@ -33,7 +42,7 @@ const СheckoutCard = () => {
             <div className={s['checkout-sidebar']}>
               <div className={s['checkout-totals']}>
                 <div>
-                  <p>Сума (3 позиції)</p>
+                  <p>Сума ({order.orderItems.length} позиції)</p>
                   <p>1050 грн</p>
                 </div>
                 <div>
@@ -43,12 +52,14 @@ const СheckoutCard = () => {
               </div>
               <div className={s.total}>
                 <p>Загальна сума</p>
-                <p>900 грн</p>
+                <p>{totalPrice} грн</p>
               </div>
             </div>
-            <button className={s.btn} onClick={() => nextStep()}>
-              Далі
-            </button>
+            {step === 1 && (
+              <button className={s.btn} onClick={() => nextStep()}>
+                Далі
+              </button>
+            )}
             <p className={s.note}>
               <span>Замітка:</span> Натискаючи «ОПЛАТИТИ», ви погоджуєтеся з нашими Умовами обслуговування, Політикою
               повернення коштів та Політикою конфіденційності.
