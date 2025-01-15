c0c8ebf52ed276599c94baa216bf730427a9577a
CegRay <vasilenkoserg510@gmail.com>
2025-01-15 16:55:21 +0200

    Small fix

--- core/apps/books/entities.py
+++ core/apps/books/entities.py
@@ -3,20 +3,24 @@ from dataclasses import (
     field,
 )
 from datetime import datetime
-from typing import Optional, List
+from typing import (
+    List,
+    Optional,
+)
+
 
 @dataclass
 class Book:
     id: Optional[int] = field(default=None, kw_only=True)  # noqa
     title: str
     current_price: int
-    old_price: Optional[int] = None
-    quantity: int = 0
+    old_price: Optional[int] = field(default=None, kw_only=True)
+    quantity: int = field(default=0, kw_only=True)
 
     description: str
     author: str
     publisher: str
-    genres: List[str] = field(default_factory=list)
+    genres: List[str] = field(default_factory=list, kw_only=True)
     publication_year: int
     country_of_origin: str
     text_language: str
@@ -26,6 +30,7 @@ class Book:
     is_available: Optional[bool] = True
     created_at: datetime = field(default_factory=datetime.utcnow)
     updated_at: Optional[datetime] = None
+
 
 @dataclass
 class OrderBook:
