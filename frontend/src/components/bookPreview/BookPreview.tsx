3b6bfd2f5507df6c20dbe642c4d85839b3483291
Masianuy <Masianuy@ukr.net>
2025-01-18 22:31:32 +0200

    top blok bookPreview

--- frontend/src/components/bookPreview/BookPreview.tsx
+++ frontend/src/components/bookPreview/BookPreview.tsx
@@ -1,8 +1,9 @@
-import { FC } from 'react'
-import { Link } from 'react-router-dom'
+import { FC, useState } from 'react'
 import { Container } from '@/shared'
 import { BookItem } from '@/utils/types/BookItemType'
 import s from './BookPreview.module.scss'
+import Tick from './Tick'
+import Star from './Star'
 
 interface BookItemProps {
   book: BookItem | null
@@ -10,6 +11,8 @@ interface BookItemProps {
 
 const BookPreview: FC<BookItemProps> = ({ book }) => {
   const { title, author, cover, rating, is_available, price } = book
+  const [rate, setRating] = useState(null)
+  const [hover, setHover] = useState(null)
   return (
     <Container>
       <div className={s.wrap}>
@@ -20,12 +23,51 @@ const BookPreview: FC<BookItemProps> = ({ book }) => {
           <p className={s.title}>{title || null}</p>
           <p className={s.author}>{author || null}</p>
           <p className={s.rating}>
-            * {`${rating}` || null}
-            <span>Оцінити</span>
+            <p className={s.rate}>
+              <Star color="#1A1D23" /> {`${rating}` || null}
+            </p>
+            <div className={s.estimate}>
+              <span>Оцінити</span>
+              <div>
+                {[...Array(5)].map((star, index) => {
+                  const currentRating = index + 1
+                  return (
+                    <label key={index}>
+                      <input
+                        key={star}
+                        type="radio"
+                        name="rating"
+                        value={currentRating}
+                        onChange={() => setRating(currentRating)}
+                      />
+                      <Star
+                        className={s.star}
+                        style={{
+                          fill: currentRating <= (hover || rate) ? '#ffc107' : '#e4e5e9',
+                        }}
+                        onMouseEnter={() => setHover(currentRating)}
+                        onMouseLeave={() => setHover(null)}
+                      />
+                    </label>
+                  )
+                })}
+              </div>
+            </div>
           </p>
-          <p className={s.available}>{is_available ? 'У наявності' : 'Розпродано'}</p>
+          <p className={s.available}>
+            {is_available ? (
+              <>
+                У наявності <Tick />
+              </>
+            ) : (
+              'Розпродано'
+            )}
+          </p>
           <p className={s.price}>{`${price || null} грн`}</p>
-          <button>До кошика</button>
+          <button className={s['add-to-cart']}>
+            <span></span>
+            До кошика
+          </button>
         </div>
       </div>
     </Container>
